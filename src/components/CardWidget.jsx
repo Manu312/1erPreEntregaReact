0ea03e381c2866f3a1dc74206699ec91c33d598c
Manu312 <manumercado14@gmail.com>
2023-12-03 18:04:51 -0300

    Fix rutas | fin entregable final

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -3,7 +3,7 @@ import { Badge, Box, Image } from "@chakra-ui/react";
 const CardWidget = () => {
     return(
         <div>
-            <Image src='src\assets\carrito.png' alt='Dan Abramov' />
+            <Image src='..src\assets\carrito.png' alt='Dan Abramov' />
             <Badge colorScheme="green">5</Badge>
         </div>
     )
